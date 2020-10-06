2000809490c615ce1ff008c6e4735cbd0e34ae68
Zhenhua yu <zyu0628@gmail.com>
2020-10-06 22:49:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,13 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager"
+    return "You are a teenager!"
   }
 }
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager"
+    return "You are a teenager!"
   }
   else {
-    return "you are not a teenager"
+    return "You are not a teenager"
   }
 }
