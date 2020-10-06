295c608e69a6457908455ccd5106c9648d361130
Zhenhua yu <zyu0628@gmail.com>
2020-10-06 22:37:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,12 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19) {
-    return "you are a teenager!"
+    return "You are a teenager!"
   }
 }
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    return "you are a teenager!"
+    return "You are a teenager!"
   }
   else {
     return "you are not a teenager!"
@@ -15,41 +15,41 @@ function teenager(age) {
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
-    return "you are a teenager!"
+    return "You are a teenager!"
   } else if (age < 12) {
-    return "you are a kid"
+    return "You are a kid"
   } else if (age > 19) {
-    return "you are a grownup!"
+    return "You are a grownup!"
   }
 
   function ternaryTeenager(age) {
-    return (age >= 13 && age <= 19) ? "you are a teenager!" : "you are not a teenager!"
+    return (age >= 13 && age <= 19) ? "You are a teenager!" : "You are not a teenager!"
   }
 
   function switchAge(age) {
     switch (age) {
       case 13:
-        return "you are a teenager"
+        return "You are a teenager"
         break;
       case 14:
-        return "you are a teenager"
+        return "You are a teenager"
         break;
       case 15:
-        return "you are a teenager"
+        return "You are a teenager"
         break;
       case 16:
-        return "you are a teenager"
+        return "You are a teenager"
         break;
       case 17:
-        return "you are a teenager"
+        return "You are a teenager"
         break;
       case 18:
-        return "you are a teenager"
+        return "You are a teenager"
         break;
       case 19:
-        return "you are a teenager"
+        return "You are a teenager"
         break;
       default:
-        return "you have an age"
+        return "You have an age"
     }
   }
